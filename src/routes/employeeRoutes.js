3d0b37af983cda2e6095fc38510364b7867bc34f
nirja <nirjadabhi34@gmail.com>
2025-10-02 00:23:45 -0400

    get employees

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -1,8 +1,23 @@
 const express = require('express');
+
+const { body, param, query} = require('express-validator');
+
+const {
+  listEmployees,
+  createEmployee,
+  getEmployeeById,
+  updateEmployeeById,
+  deleteEmployeeByQuery
+} = require('../controllers/employeeController');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Employee route');
 });
+
+router.get("/employees", listEmployees);
+
+
 
 module.exports = router;
