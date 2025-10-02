3a28b9d2117993e3692fe82ef92052b53ea6feb5
nirja <nirjadabhi34@gmail.com>
2025-10-02 00:19:15 -0400

    signup api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const { body } = require('express-validator');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('User route');
+router.post('/signup', (req, res) => {
+  body('username').trim().notEmpty().withMessage('Username is required');
+  body('email').trim().isEmail().withMessage('Invalid email format');
+  body('password').trim().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long');
 });
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
